2c9cdb80cad9fd06f4778dc8841a7fb53114f43d
Wayne Hoover <wayne@blissofbeing.com>
2012-07-11 09:25:36 -0500

    no more unsafewindow calls, update reademe

---
+++ README.md
@@ -0,0 +1,13 @@
+##Hype Machine direct download links v3
+This userscript script adds a download link next to each track on The Hype Machine (www.hypem.com)
+
+Its available on userscripts here: http://userscripts.org/scripts/show/112466
+
+It works with Greasemonkey on firefox and Tapermonkey on Chrome
+
+##Credits
+Thank you to the follow people who's code I have adapted into this script one way or another, without this script wouldn't exist.
+
+@tonyskn
+@phr33ksho
+@obmas
--- hypem.js
+++ hypem.js
@@ -1,44 +1,46 @@
 // ==UserScript==
 // @name           Hype Machine direct download links v3
 // @author         @blissofbeing
-// @version        0.2.0
-// @description	   Add download links next to tracks on The Hype Machine.
+// @version        0.2.1
+// @description    Add download links next to tracks on The Hype Machine.
 // @include        http://hypem.com/*
 // ==/UserScript==
 // Add "Download" link next to songs on the HypeMachine ( http://hypem.com )
+
+function main(){
 
 var TrackList;
 var TrackElements;
 var SelectedTrack;
 
 function addLinks() {
-	TrackList = unsafeWindow.trackList[document.location.href];
-	if (TrackList == undefined || TrackList.length < 1) {
+	TrackList = trackList[document.location.href];
+	if (TrackList === undefined || TrackList.length < 1) {
 		//GM_log('delay');
-		unsafeWindow.setTimeout(addLinks, 1000);
+		setTimeout(addLinks, 1000);
 	} else {
 		// Check if this particular page has been processed through a previous call
-		if (unsafeWindow.jQuery('.gmlink').length < 1) {
-			TrackElements = unsafeWindow.jQuery('div.section-track');
+		if (jQuery('.gmlink').length < 1) {
+			TrackElements = jQuery('div.section-track');
 			SelectedTrack = 0;
 			var index = 0;
-			var tracks = unsafeWindow.jQuery('div.section-track .track_name');
+			var tracks = jQuery('div.section-track .track_name');
 			tracks.each(function(index, element) {
 				var trackId = TrackList[index].id;
 				var trackKey = TrackList[index].key;
 				var trackArtist = TrackList[index].artist;
 				var trackSong = TrackList[index].song;
 				if (trackKey) {
-						if(element.innerHTML.indexOf("Download")==-1){																												
+						if(element.innerHTML.indexOf("Download")==-1){
 							var ahref = document.createElement('a');
 							ahref.setAttribute('href', "/serve/play/"+trackId+"/"+trackKey);
 							ahref.setAttribute('style','color:red;position: absolute;right: 10px;top: -15px;');
 							ahref.setAttribute('title',trackArtist+" - "+trackSong);
 							ahref.setAttribute('class','gmlink');
-							ahref.appendChild(document.createTextNode("Download"));							
+							ahref.appendChild(document.createTextNode("Download"));
 							element.parentNode.insertBefore( ahref, element.nextSibling );
 						}
-				}		
+				}
 				index++;
 			});
 		}
@@ -48,7 +50,13 @@ function addLinks() {
 addLinks();
 
 // Display links after an Ajax update is complete
-unsafeWindow.jQuery(document).ajaxComplete(function() {
+jQuery(document).ajaxComplete(function() {
   addLinks();
   jQuery(".section.same .tools").css('top','29px');
 });
+
+}
+    
+var script = document.createElement('script');
+script.appendChild(document.createTextNode('(' + main + ')();'));
+(document.body || document.head || document.documentElement).appendChild(script);
