48bac696cee26c25e05da6d75c22ba1b53e1a5ff
Wayne Hoover <wayne@blissofbeing.com>
2012-07-16 10:30:33 -0500

    fix links not appearing sometimes after ajax calls

--- hypem.js
+++ hypem.js
@@ -1,6 +1,15 @@
 // ==UserScript==
 // @name           Hype Machine direct download links v3
 // @author         @blissofbeing
+// @version        0.2.2
+// @description    Add download links next to tracks on The Hype Machine.
+// @include        http://hypem.com/*
+// ==/UserScript==
+// Add "Download" link next to songs on the HypeMachine ( http://hypem.com )
+
+// ==UserScript==
+// @name           Hype Machine direct download links v3
+// @author         @blissofbeing
 // @version        0.2.1
 // @description    Add download links next to tracks on The Hype Machine.
 // @include        http://hypem.com/*
@@ -16,34 +25,31 @@ var SelectedTrack;
 function addLinks() {
 	TrackList = trackList[document.location.href];
 	if (TrackList === undefined || TrackList.length < 1) {
-		//GM_log('delay');
 		setTimeout(addLinks, 1000);
 	} else {
-		// Check if this particular page has been processed through a previous call
-		if (jQuery('.gmlink').length < 1) {
-			TrackElements = jQuery('div.section-track');
-			SelectedTrack = 0;
-			var index = 0;
-			var tracks = jQuery('div.section-track .track_name');
-			tracks.each(function(index, element) {
+		TrackElements = jQuery('div.section-track');
+		SelectedTrack = 0;
+		var index = 0;
+		var tracks = jQuery('div.section-track .track_name');
+		tracks.each(function(index, element) {
+			var $element = jQuery(element);
+			if ( !$element.parent().find('a.gmlink').length ){ // Check if this particular element has alrdeady been processed through a previous call
 				var trackId = TrackList[index].id;
 				var trackKey = TrackList[index].key;
 				var trackArtist = TrackList[index].artist;
 				var trackSong = TrackList[index].song;
-				if (trackKey) {
-						if(element.innerHTML.indexOf("Download")==-1){
-							var ahref = document.createElement('a');
-							ahref.setAttribute('href', "/serve/play/"+trackId+"/"+trackKey);
-							ahref.setAttribute('style','color:#ed2024;position: absolute;right: 10px;top: -15px;');
-							ahref.setAttribute('title',trackArtist+" - "+trackSong);
-							ahref.setAttribute('class','gmlink');
-							ahref.appendChild(document.createTextNode("Download"));
-							element.parentNode.insertBefore( ahref, element.nextSibling );
-						}
+				if (trackKey ) {
+					jQuery('<a/>', {
+						'href': "/serve/play/"+trackId+"/"+trackKey, //or use "/serve/f/509/"+trackId+"/"+trackKey for urls not bound to session.
+						'style': 'color:#ed2024;position: absolute;right: 10px;top: -15px;',
+						'title': trackArtist+" - "+trackSong,
+						'class': 'gmlink',
+						'text': 'Download'
+					}).appendTo($element.parent());
 				}
 				index++;
-			});
-		}
+			}
+		});
 	}
 }
 
@@ -54,8 +60,8 @@ jQuery(document).ajaxComplete(function() {
   addLinks();
   jQuery(".section.same .tools").css('top','29px');
 });
-
-}
+    
+} // /main
     
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('(' + main + ')();'));
